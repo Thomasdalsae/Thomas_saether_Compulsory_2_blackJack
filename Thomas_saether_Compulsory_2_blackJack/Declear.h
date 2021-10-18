fa0716cd8f43facfe255220df48b9f4120a3fc3d
Thomasdalsae <72367829+Thomasdalsae@users.noreply.github.com>
2021-10-18 23:52:16 +0200

    Comments

--- Thomas_saether_Compulsory_2_blackJack/Declear.h
+++ Thomas_saether_Compulsory_2_blackJack/Declear.h
@@ -7,14 +7,14 @@
 #include <fstream>
 #include <random> 
 #include <algorithm>
-
-
-
-bool blackJack();
+//for loginchecker, want to uset the login profiles name as player.
+std::vector<std::string>playerLogin{};
+//made a struct to hold all the information the cards need, there are 4 different types of cards with 13 cards each. 
 struct cards
 {
 	int cardsType{};
 	int cardNr{};
+	//Here we have some contructors for our cards.
 	cards(int type, int number) {
 		cardsType = type;
 		cardNr = number;
@@ -22,12 +22,32 @@ struct cards
 
 };
 
+//Global playerbank to keep track of currecny of both the player and the house.
 std::vector<int> playerBank(2, 100);
-int HouseAi(std::vector<cards> , std::vector<int>&, int drawTracker);
-
-
 bool loginChecker();
+//made a function for just the AI, it takes inn the variables noted inn the syntax below.
+int HouseAi(std::vector<cards>, std::vector<int>&, int drawTracker);
 int txtInfo;
+
+
+
+
+//This is all the variables used for the blackJack function
+bool blackJack();
+char input{};
+//takes inn the players input to actives different parts of the code.
+	int playerChoice{};
+
+	int playerTurn{};
+	int drawTracker{};
+	int Pool{};
+	std::vector<cards> Card_v;
+	std::vector<int> playerSum(2, 0);
+//---------------------------------------------------------------
+
+
+
+
 
 
 
--- Thomas_saether_Compulsory_2_blackJack/main.cpp
+++ Thomas_saether_Compulsory_2_blackJack/main.cpp
@@ -1,12 +1,9 @@
 #include "Declear.h"
 
 
-
-
-
-
-
-bool main() {
+//login is Thomas
+//password is 123321
+int main() {
 	std::vector<int> playerBank(2, 100);
 	if (loginChecker() == true)
 
@@ -17,18 +14,12 @@ bool main() {
 		blackJack();
 	}
 	
-	return false;
+
 
 }
 bool blackJack(){
-	char input{};
-	int playerChoice{};
-	int playerTurn{};
-	int drawTracker{};
-	int Pool{};
-	std::vector<cards> Card_v;
-	std::vector<int> playerSum(2, 0);
 
+	//making the vector for the cards.
 	while (true)
 	{
 		
@@ -43,30 +34,41 @@ bool blackJack(){
 		}
 	}
 
-	
+	//this is a checker for the banks of both the player and the house. they need to have more than 10.
 	if (playerBank[0] < 10)
 	{
 		std::cout << "looks like you are out of money!" << std::endl;
 		exit(0);
 	}
+	if (playerBank[1] < 10)
+	{
+		std::cout << "House is out of money, you win this time!" << std::endl;
+		exit(0);
+	}
 	
-	
-	std::cout << "players bank :" << playerBank[0] << std::endl;
+	//showing the banks of each player
+	std::cout << playerLogin[0] << " bank :" << playerBank[0] << std::endl;
 	std::cout << "houses bank :" << playerBank[1] << std::endl;
 
 	
-
-	std::cout << "Buy inn is 10$, Do you want to continue?" << std::endl;
+	//making the players pay 10$.
+	std::cout << "Buy inn is 10$, Do you want to continue? y/n  :" << std::endl;
 	std::cin >> input;
 
-	switch (toupper(input))
+	//forcing the input from the player to become uppercase, makes it easier to use the switch case.
+	char blackJackAnswer = toupper(input);
+
+	switch (blackJackAnswer)
+
 	{
-	case 'y':
+	case 'Y':
 	{
-		continue;
+		system("cls");
+		break;
+		
 	}
 	break;
-	case 'n':
+	case 'N':
 	{
 		exit(0);
 	}
@@ -74,29 +76,35 @@ bool blackJack(){
 	default:
 		break;
 	}
-
+	//setting up the correct values for banks and players current card sum. also adding the buyinn to the pool.
 		playerBank[0] -= 10;
 		playerBank[1] -= 10;
 		playerSum[0] = 0;
 		playerSum[1] = 0;
 		Pool += 20;
-		
+		//randomizer that i use inn the shuffle.
 		std::random_device rd;
 		std::mt19937_64 g(rd());
 
 
 		while (true)
 		{
+			//shuffle takes all the cards within the vector and (suprise) shuffles it. 
+			//(Card_v.begin(), Card_v.end() tells the shuffle where to start and where it ends. this is easiest with vector
+	
 		std::shuffle(Card_v.begin(), Card_v.end(), g);
-
+		Pool = 0;
 		std::cout << "Do you want to draw card? y/n  :" << std::endl;
 		std::cin >> input;
 		system("cls");
 			if (toupper(input == 'y'))
 			{
+				//the drawTracker is very important, it keeps track on where inn the vector the player currently are
+				//i use this information and send it to my ai function, this makes it continue where the player left off in the vector.
 				drawTracker++;
 				for (size_t i = 0; i < 1; i++)
 				{
+					//if the player draws a ace he/she can choose between 1 and 11.
 					if (Card_v[i].cardNr == 1)
 					{
 						std::cout << "Do you want the card to count as 1 or 11 ?: 1/11";
@@ -106,7 +114,7 @@ bool blackJack(){
 						{
 						case 11:
 						{
-							std::cout << "Player choose 11" << std::endl;
+							std::cout << playerLogin[0] << " choose 11" << std::endl;
 							std::cout << playerSum[0] << "+" << "11 = ";
 							playerSum[0] += 11;
 							std::cout << playerSum[0] << std::endl;
@@ -115,7 +123,7 @@ bool blackJack(){
 						break;
 						case 1:
 						{
-							std::cout << "Player choose 1" << std::endl;
+							std::cout << playerLogin[0] << " choose 1" << std::endl;
 							std::cout << playerSum[0] << "+" << "1 = ";
 							playerSum[0] += 1;
 							std::cout << playerSum[0] << std::endl;
@@ -128,6 +136,7 @@ bool blackJack(){
 
 
 					}
+					//if the card that is drawn is over 10 it will automatically become card value 10.
 					if (Card_v[i].cardNr >= 10)
 					{
 
@@ -136,11 +145,9 @@ bool blackJack(){
 						playerSum[0] += 10;
 
 					}
+					//every other card uses their given number ans value.
 					else if (Card_v[i].cardNr < 10 && Card_v[i].cardNr > 1)
 					{
-						//Hvilken type å nr
-						//std::cout << Card_v[i].cardsType << " " ;
-						//std::cout << Card_v[i].cardNr << std::endl;
 
 						std::cout << playerSum[0] << "+" << Card_v[i].cardNr << " = ";
 						playerSum[0] += Card_v[i].cardNr;
@@ -149,26 +156,28 @@ bool blackJack(){
 					std::cout << playerSum[0] << std::endl;
 				}
 
-
+				//this is a checker for player, if you go over 21 the house wins.
 				if (playerSum[0] > 21)
 				{
 					system("cls");
-					std::cout << "Player went to high...House wins:" << std::endl;
+					std::cout << playerLogin[0] << " went to high...House wins:" << std::endl;
 					playerBank[1] += 20;
 					return false;
 				}
 
 			}
+			//when the player is happy with the current cardsum he can decide to hold.
 		else if (toupper(input == 'n'))
 		{
 
-		std::cout << "Player one holds" << playerSum[0] << std::endl;
+		std::cout << playerLogin[0] <<" holds" << playerSum[0] << std::endl;
 
 		clearCin();
 
 		std::cout << "How much do you want to bet ? the house will match you.";
 		std::cin >> playerChoice;
-		while (true)
+		//this is a saftey check for betting to high or low. both for the players and house banks.
+		while (playerChoice > playerBank[0] || playerChoice < playerBank[0] || playerChoice > playerBank[1] || playerChoice < playerBank[1])
 		{
 
 			if (playerChoice > playerBank[0] && playerChoice > playerBank[1])
@@ -184,12 +193,14 @@ bool blackJack(){
 
 		}
 		system("cls");
-		std::cout << "Player betted:" << playerChoice;
+		std::cout << playerLogin[0] <<" betted :" << playerChoice;
+		//after the player have betted the Ai function starts. it takes inn the information from the players game.
+		//the card vector which hold all the card, the current players value and where he is in the vector.
 		HouseAi(Card_v, playerSum, drawTracker);
 		break;
 		}
 		}
-		
+		//if the house gets the same cardvalue as the player it becomes a draw and the player are given the option to play again.
 		if (playerSum[1] == playerSum[0])
 		{
 			system("cls");
@@ -201,7 +212,7 @@ bool blackJack(){
 		}
 		if (true)
 		{
-
+			//takes the betted amout out of the players and houses bank and adds it to the pool.
 	playerBank[0] -= playerChoice;
 	playerBank[1] -= playerChoice;
 
@@ -211,16 +222,16 @@ bool blackJack(){
 
 	if (playerSum[1] > 21)
 	{
-		std::cout << "house went to high... player wins" << std::endl;
+		std::cout << "house went to high..." << playerLogin[0] << " wins" << std::endl;
 		playerBank[0] += Pool;
 		return false;
 		
 	}
 
-	if (playerSum[playerTurn] > playerSum[1])
+	if (playerSum[0] > playerSum[1] && playerSum[0] <= 21)
 	{
 		playerBank[0] += Pool;
-		std::cout << "congratulations player, you won against the house :" << Pool << std::endl;
+		std::cout << "congratulations " << playerLogin[0] <<" you won against the house : " << Pool << std::endl;
 		
 	}
 	else
@@ -241,27 +252,14 @@ bool blackJack(){
 
 
 
-	
-	
-	
 
 	return false;
 
 }
 
-	
-	
-	
 
-
-	/*std::copy(Card_v.begin(), Card_v.end(), / std::ostream_iterator<int>(std::cout, " "));
-	std::cout << "\n";*/
-
-
-
-	
-	
-
+//this is basically the same as the blackJack funtion wihtout the cout.
+//the AI will always try to get draw or better than the player.
 int HouseAi(std::vector<cards> Card_v, std::vector<int>& playerSum, int drawTracker) {
 	while (true)
 	{
@@ -275,6 +273,7 @@ int HouseAi(std::vector<cards> Card_v, std::vector<int>& playerSum, int drawTrac
 			drawTracker++;
 			if (Card_v[i].cardNr == 1)
 			{
+				//if ai can us 11 from the ace without going above 21 it will.
 				if (playerSum[1] < 11)
 				{
 					std::cout << playerSum[1] << "+" << "11 = ";
@@ -325,15 +324,19 @@ int HouseAi(std::vector<cards> Card_v, std::vector<int>& playerSum, int drawTrac
 
 
 
-
+//this is the login checker, it checks the input from the player. if it matches the username and password from the "server"
+//it will give them acces to play.
 bool loginChecker() {
-
 	
+	//gives the user 3 chances to login
 	for (size_t i = 0; i < 3; i++)
 	{
 
-	std::vector<std::string>playerLogin;
-	
+
+	//saves the attempts to the templogin
+		//checks the text file
+		//take one and one line from the txt
+		//after the 9th char it will start the check.
 	std::string tempLogin;
 	std::ifstream infile("loginInfo.txt");
 	std::string line{};
@@ -344,7 +347,7 @@ bool loginChecker() {
 	std::cin >> tempLogin;
 	playerLogin.push_back(tempLogin);
 	
-	
+	//if the username matches the line from the txt file it will give them the chance to write inn the password
 	if (playerLogin[0] == line1)
 	{
 		std::getline(infile, line);
@@ -372,10 +375,6 @@ bool loginChecker() {
 	
 		}
 
-	
-	
-
-	
 	return {};
 	}
 
